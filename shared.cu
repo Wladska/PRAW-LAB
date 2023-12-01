88a65dbf27301b0b68c7d7a874b107565cafd2ef
Popularkiya <kingawladzinsk@gmail.com>
2023-12-01 22:52:56 +0100

    shared mem impl

--- shared.cu
+++ shared.cu
@@ -4,6 +4,7 @@ CUDA - generation and sum of arithmetic progression build of 10240000 elements a
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 #include <cuda_runtime.h>
 
 __host__
@@ -14,19 +15,20 @@ void errorexit(const char *s) {
 
 //elements generation
 __global__ 
-void calculate(long *result) {
+void calculate(float *result) {
     //allocate memory - size same as number of threads in block
-   __shared__ long sresults[1024];  
-    int counter;
+   __shared__ float sresults[1024];  
     int my_index=blockIdx.x*blockDim.x+threadIdx.x;
-    //write in shared memory element value for current thread
-    sresults[threadIdx.x]=my_index;
+
+    sresults[threadIdx.x]= 1 / powf(2, my_index);
     __syncthreads();
+
     //calculate sum of all elements of thread within same block using shared memory 
-   for(counter=512;counter>0;counter/=2) {
-      if (threadIdx.x<counter)
-        sresults[threadIdx.x]=(sresults[threadIdx.x]+sresults[threadIdx.x+counter]);
-      __syncthreads();      
+   for (unsigned int s = 1; s < blockDim .x; s *= 2) {
+      if ( threadIdx.x % (2 * s) == 0) {
+        sresults[ threadIdx.x ] += sresults[ threadIdx.x + s ];
+      }
+      __syncthreads ();   
     }
 
     //first thread in block write results of this block to global memory
@@ -35,40 +37,40 @@ void calculate(long *result) {
     }
 }
 
-
 int main(int argc,char **argv) {
 
-    long long result;
+    float result;
     int threadsinblock=1024;
-    int blocksingrid=10000; 
+    int blocksingrid=10000;	
 
     int size = threadsinblock*blocksingrid;
-    
     //memory allocation on host
-    long *hresults=(long*)malloc(blocksingrid*sizeof(long));
-    if (!hresults) errorexit("Error allocating memory on the host");  
-    
-     //devie memory allocation (GPU)
-    long *dresults=NULL;
-    if (cudaSuccess!=cudaMalloc((void **)&dresults,blocksingrid*sizeof(long)))
+    float *hresults=(float*)malloc(size*sizeof(float));
+    if (!hresults) errorexit("Error allocating memory on the host");	
+
+    float *dresults=NULL;
+    //devie memory allocation (GPU)
+    if (cudaSuccess!=cudaMalloc((void **)&dresults,size*sizeof(float)))
       errorexit("Error allocating memory on the GPU");
-    
+
     //call to GPU - kernel execution 
     calculate<<<blocksingrid,threadsinblock>>>(dresults);
     if (cudaSuccess!=cudaGetLastError())
       errorexit("Error during kernel launch");
-     
-      //getting results from GPU to host memory
-    if (cudaSuccess!=cudaMemcpy(hresults,dresults,blocksingrid*sizeof(long),cudaMemcpyDeviceToHost))
+  
+    //getting results from GPU to host memory
+    if (cudaSuccess!=cudaMemcpy(hresults,dresults,size*sizeof(float),cudaMemcpyDeviceToHost))
        errorexit("Error copying results");
+
 
     //calculate sum of all elements
     result=0;
-    for(int i=0;i<blocksingrid;i++) {
-      result = result + hresults[i];
+
+    for(int i=0; i<blocksingrid; i++) {
+      result = result + hresults[i * threadsinblock];
     }
 
-    printf("\nThe final result is %lld\n",result);
+    std::cout << "\nThe final result is " << result << std::endl;
 
     //free memory
     free(hresults);
@@ -76,3 +78,4 @@ int main(int argc,char **argv) {
       errorexit("Error when deallocating space on the GPU");
 
 }
+
