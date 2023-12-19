d4cc7b85b470357f6c640fdb4d4064a767f23a5d
Popularkiya <kingawladzinsk@gmail.com>
2023-12-19 20:39:47 +0100

    lab4 init

---
+++ unified.cu
@@ -0,0 +1,61 @@
+/*
+CUDA - generation and sum of arithmetic progression build of 10240000 elements a1=0 r=1 with Unified Memory
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda_runtime.h>
+#include <cmath>
+
+__host__
+void errorexit(const char *s) {
+    printf("\n%s",s);	
+    exit(EXIT_FAILURE);	 	
+}
+
+//elements generation
+__global__ 
+void calculate(int *result, unsigned long long NUMBER, unsigned long long BIGGEST_NUMBER_TO_CHECK) {
+    int my_index=blockIdx.x*blockDim.x+threadIdx.x;
+    if (my_index >= 2 && my_index <= BIGGEST_NUMBER_TO_CHECK && NUMBER % my_index == 0) {
+        atomicAdd(&(result[blockIdx.x]), 1);
+    }
+}
+
+
+int main(int argc,char **argv) {
+    unsigned long long NUMBER  = 7830817777;
+    unsigned long long BIGGEST_NUMBER_TO_CHECK  = sqrt(7830817777);
+    int threadsinblock = 1024;
+    int blocksingrid= BIGGEST_NUMBER_TO_CHECK / threadsinblock;	
+
+    int *results;
+
+    //unified memory allocation - available for host and device
+    if (cudaSuccess!=cudaMallocManaged(&results,blocksingrid*sizeof(int)))
+      errorexit("Error allocating memory on the GPU");
+
+    //call to GPU - kernel execution 
+    calculate<<<blocksingrid,threadsinblock>>>(results, NUMBER, BIGGEST_NUMBER_TO_CHECK);
+
+    if (cudaSuccess!=cudaGetLastError())
+      errorexit("Error during kernel launch");
+  
+    //device synchronization to ensure that data in memory is ready
+    cudaDeviceSynchronize();
+
+    //calculate sum of all elements
+    int result=0;
+    for(int i=0;i<size;i++) {
+      result += results[i];
+    }
+
+    if (result == 0) {
+      printf("\nNumber IS prime",result);
+    } else {
+      printf("\nNumber IS NOT prime");
+    }
+
+    //free memory
+    if (cudaSuccess!=cudaFree(results))
+      errorexit("Error when deallocating space on the GPU");
+}
